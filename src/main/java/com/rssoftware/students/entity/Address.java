20787785b09c4544a6db9f3015a2e79354d39152
Souvik Sarkar <SouvikS@rssoftware.com>
2024-10-24 15:15:24 +0530

    Address Entity

---
+++ src/main/java/com/rssoftware/students/entity/Address.java
@@ -0,0 +1,67 @@
+package com.rssoftware.students.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String street;
+    private String city;
+    private String state;
+    private Integer pin;
+
+    public Address() {
+    }
+
+    public Address(String street, String city, String state, int pin) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.pin = pin;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+}
