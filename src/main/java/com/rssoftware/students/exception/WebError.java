1388fd9756a8c6840747213edb5c9ba48f07b793
Souvik Sarkar <SouvikS@rssoftware.com>
2024-10-24 15:23:48 +0530

    Custom Error Object

---
+++ src/main/java/com/rssoftware/students/exception/WebError.java
@@ -0,0 +1,37 @@
+package com.rssoftware.students.exception;
+
+public class WebError {
+    private int statusCode;
+    private String message;
+    private String details;
+
+    public WebError(int statusCode, String message, String details) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.details = details;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
