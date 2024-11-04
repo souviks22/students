cb553300d10e060420945c74894bab56a531e92a
Souvik Sarkar <SouvikS@rssoftware.com>
2024-11-04 11:50:06 +0530

    Changed Report Structure

--- src/main/java/com/rssoftware/students/service/ReportService.java
+++ src/main/java/com/rssoftware/students/service/ReportService.java
@@ -5,7 +5,9 @@ import com.rssoftware.students.repository.StudentRepository;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
@@ -18,11 +20,13 @@ public class ReportService {
     @Autowired
     private StudentRepository studentRepository;
 
+    @Value("classpath:reports/students.jrxml")
+    private Resource reportResource;
+
     public byte[] getStudentsPDFByFilter(String name, Integer ageUp, Integer ageDown, Integer standardUp, Integer standardDown, String street, String city, String state, String pin) {
         List<FlatStudent> students = studentRepository.getFlatStudentsByFilter(name, ageUp, ageDown, standardUp, standardDown, street, city, state, pin);
         try {
-            InputStream in = new ClassPathResource("/reports/students.jrxml").getInputStream();
-            JasperReport report = JasperCompileManager.compileReport(in);
+            JasperReport report = JasperCompileManager.compileReport(reportResource.getInputStream());
             JRBeanCollectionDataSource data = new JRBeanCollectionDataSource(students);
             Map<String, Object> params = new HashMap<>();
             JasperPrint structure = JasperFillManager.fillReport(report, params, data);
